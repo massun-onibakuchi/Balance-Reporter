1ceabd26b9dd122b4ebb282c51e5fac709dc6c20
bakuchi- <helveticaf0015se@gmail.com>
2020-11-12 21:49:24 +0900

    fix createBody

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { request } from "http";
+
 const CCXT = require('ccxt');
 const { initExchange } = require('../exchange');
 const { sheetAPI, append, batchUpdate, get } = require('../sheet');
@@ -7,7 +9,7 @@ const spreadsheetId = process.env.spreadsheetId;
 const range = 'Wallet!B1:E';
 enum RequestType {
     Append,
-    BatchUpdate,
+    Update,
     Get
 }
 const labelRequest = {
@@ -18,7 +20,65 @@ const labelRequest = {
     dateTimeRenderOption: 'SERIAL_NUMBER',
 };
 
-const createWalletDate = (label: string[], balance: {}): [string[], number[]] => {
+const createRequestBody = (ranges: string[]): unknown => {
+    const labelRange = ranges[0] || 'Wallet!A1:1';
+    const appendRange = ranges[1] || 'Wallet!B1:1';
+    const updateRange = ranges[2] || 'Wallet!A1:1';
+    const labelRequest = {
+        spreadsheetId: spreadsheetId,
+        range: appendRange,
+        majorDimension: "ROWS",
+        valueRenderOption: 'FORMATTED_VALUE',
+        dateTimeRenderOption: 'SERIAL_NUMBER'
+    }
+    const appendRequest = {
+        spreadsheetId: spreadsheetId,
+        range: appendRange,
+        insertDataOption: 'INSERT_ROWS',
+        valueInputOption: 'USER_ENTERED',
+        resource: {
+            values: []
+        }
+    }
+    const updateRequest = {
+        spreadsheetId: spreadsheetId,
+        resource: {
+            valueInputOption: 'USER_ENTERED',
+            data: {
+                range: updateRange,
+                majorDimension: "ROWS",
+                values: []
+            },
+        }
+    }
+    const requests = [labelRequest, appendRequest, updateRequest];
+    function requestBody(req, row) {
+        if (req == RequestType.Get) return labelRequest;
+        if (req == RequestType.Append) {
+            appendRequest.resource.values = row
+            return appendRequest
+        }
+        if (req == RequestType.Append) {
+            updateRequest.resource.data.values = row
+            return updateRequest
+        }
+    }
+    return {
+        requestBody(request) { requests[request] },
+        setRange(type: string | number, range: any) { this[type] = range },
+        getRequestBody: requestBody
+    }
+}
+
+// Class RequestBody {
+//     constructor(){
+//         this.appendRange = 'Wallet!B1:1';
+//         this.labelRange = 'Wallet!A1:1';
+//         this.updateRange = 'Wallet!A1:1';
+//     }
+// }
+
+const createWalletDate = (label: string[], balance: Object): [string[], number[]] => {
     const wallet = label.reduce((acc, elem) => {
         acc[elem] = 0
         return acc
