a03669c7278ed677f277ec364471c67617eaf707
bakuchi- <helveticaf0015se@gmail.com>
2020-11-25 20:21:35 +0900

    fix expectedReturn

--- src/playground/arbitrage.ts
+++ src/playground/arbitrage.ts
@@ -1,9 +1,10 @@
-import CCXT, { bitbank } from 'ccxt';
+import CCXT from 'ccxt';
 import axiosBase from 'axios';
 const { initExchange } = require('../exchange');
 const exchange = initExchange(CCXT, undefined, 'ftx') as CCXT.Exchange;
+import arbitrageConfig from './arbitrageConfig.json';
 
-const symbols = ['BTC/USD', 'ETH/USD', 'XRP/USD'];
+const symbols = ['BTC', 'ETH', 'XRP'];
 
 interface Prices {
     symbol: {
@@ -20,8 +21,9 @@ interface Tickers {
         bid: number;
         cask: number | null;
         cbid: number | null;
-        bask?: number | null;
-        bbid?: number | null;
+        bask: number | null;
+        bbid: number | null;
+        rate: number | null;
         [excessProperty: string]: any;
     }
 }
@@ -30,59 +32,91 @@ const bb = new CCXT['bitbank']();
 
 (async () => {
 
-    const res = await exchange.fetchTickers(symbols) as Prices;
+    const res = await exchange.fetchTickers(symbols.map(el => el + '/USD')) as Prices;
 
     const assignTickers = (prices: Prices, target: any): Tickers => {
         const tickers = {};
         for (const [key, value] of Object.entries(prices)) {
             tickers[key] = {
-                ask: value["info"]["ask"],
-                bid: value["info"]["bid"],
+                ask: value["ask"],
+                bid: value["bid"],
                 cask: null,
                 cbid: null,
+                bask: null,
+                bbid: null,
+                rate: null
             }
         }
         return Object.assign(target || {}, tickers)
     }
-    const tickers = assignTickers(res, {});
-    console.log('tickers :>> ', tickers);
+    let tickers = assignTickers(res, {});
 
-    const addCPrices = async (tickers: Tickers, base: string): Promise<Tickers> => {
-        const baseRate = (await axiosBase.get('https://api.exchangeratesapi.io/latest?base=' + base)).data.rates[base.toUpperCase()]
+    const addCPrices = async (tickers: Tickers, base: string, target: string): Promise<Tickers> => {
+        const baseRate = (await axiosBase.get('https://api.exchangeratesapi.io/latest?base=' + base.toUpperCase())).data.rates[target.toUpperCase()]
+        console.log(`${base.toUpperCase()}/${target.toUpperCase()}:${baseRate}`);
+
         for (const [, value] of Object.entries(tickers)) {
             value["cask"] = value["ask"] * baseRate;
             value["cbid"] = value["bid"] * baseRate;
+            value["rate"] = baseRate;
         }
         return tickers;
     }
-    const addBPrices = async (tickers: Tickers, exchange: CCXT.Exchange, symbols): Promise<Tickers> => {
-        const res = await exchange.fetchTickers(symbols) as Prices;
-        for (const [, value] of Object.entries(tickers)) {
-            value["bask"] = res["info"]["ask"];
-            value["bbid"] = res["info"]["bid"];
+    const addBPrices = async (tickers: Tickers, exchange: CCXT.Exchange, symbols: string[], base): Promise<Tickers> => {
+        for (const symbol of symbols) {
+            const res = await exchange.fetchTicker(symbol)
+            tickers[symbol.replace('/JPY', '/USD')]["bask"] = res["ask"];
+            tickers[symbol.replace('/JPY', '/USD')]["bbid"] = res["bid"];
         }
         return tickers;
     }
 
-    await addCPrices(tickers, 'USD');
-    await addBPrices(tickers, bb, symbols.map((el) => el.replace('/USD', '')))
-        .then((result) => {
-            console.log(result);
-        }).catch((err) => {
+    await addCPrices(tickers, 'USD', 'JPY');
+    tickers = await addBPrices(tickers, bb, symbols.map(el => el + '/JPY'), 'USD');
 
-        });
-    // const judgeOp = (tikers,level)=>{
+    const expectedReturn = (tickers: Tickers, tradeConfig: { [x: string]: any; }) => {
+        const calculator = {
+            diffPercent: function () {
+                return (typeof this.sellBasedJPY == 'number') ? 100 * (this.sell / this.buy - 1) : NaN
+            },
+            quantity: 0,
+            tradeFeePercent: null,
+            sendFeeCrypto: null,
+            sendFeeJPY: function () {
+                return this.sendFee * this.buy;
+            },
+            totalmoney: function () { return this.buy * this.quantity },
+            profit: function () {
+                return this.quantity * (this.diffPercent() * this.buy - this.tradeFeePercent * this.sellBasedJPY) / 100 - this.sendFeeCrypto * this.buy
+            },
+            expectedReturn: function () {
+                return 100 * this.profit() / this.totalmoney
+            }
+        };
+        for (const [key, value] of Object.entries(tickers)) {
+            value["buy"] = value["cask"] - value["bbid"] > 0 ? value["bbid"] : null
+            value["sellBasedUSD"] = value["cask"] - value["bbid"] > 0 ? value["ask"] : null
+            value["sellBasedJPY"] = value["cask"] - value["bbid"] > 0 ? value["ask"] * value["rate"] : null
+            value.assign(tradeConfig[key]);
+        }
+        Object.assign(calculator, Object.assign(tickers));
+    }
 
+    console.log('expectedReturn(tickers,) :>> ', expectedReturn(tickers, arbitrageConfig));
+
+    // function TradeConfig(quantity, tradeFee, sendFeeCrypto) {
+    //     this.quantity = quantity
+    //     this.tradeFee = tradeFee
+    //     this.sendFeeCrypto = sendFeeCrypto
     // }
-    // const expectedReturn = ()=>{
 
-    // }
+}
     // const res = (await axios.get('indexes/DEFI/weights')).data.result
-    // console.log('res :>> ', res);
+    // console.log('res 1 res);
 
     // const defiIndex = [];
     // for (const key in res) {
     //     defiIndex.push(key+'/USD');
     // }
 
-})()
+}) ()
