6e5ccb986f55f1378f44ca84e1a9fad9ccfe91fa
bakuchi- <helveticaf0015se@gmail.com>
2020-11-26 11:33:57 +0900

    move files

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,9 @@ export = {
         "APIKEY": process.env.FTX_APIKEY,
         "APISECRET": process.env.FTX_APISECRET
     },
-    "LINE":{
-        "BEAR_ACCESS_TOKEN":"fwgOz8phRpJL34EIITrovrc7IJgr5LOn2Lo8ftF3g1/6w1ltvIJrMJ97cLO0ceeIDDpL8PG5751dEsT9cJus8F5kkDWk9Md+bvvHbX1jPN2VOIh5NA/DO+cXcZK3B3W+lls4/wZ30IXZhMSxdbYDCgdB04t89/1O/w1cDnyilFU=",
-        "USER_ID":"U1e040d8720cfc354ca413dc75550b56d"
+    "LINE": {
+        "BEAR_ACCESS_TOKEN": process.env.BEARER_ACCESS_TOKEN,
+        "USER_ID": process.env.USER_ID
 
     }
 }
